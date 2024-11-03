224b9ef49f170044d073e363605edd3d539532e7
mariia tokhtamysh <mariyka.tokhtamysh@gmail.com>
2024-11-03 20:42:25 +0200

    add total sum of squared deviations

--- Dispersion_Analysis/Program.cs
+++ Dispersion_Analysis/Program.cs
@@ -22,9 +22,12 @@ class Program
         Console.WriteLine($"Від 3 до 5 км: {avg2:F2}");
         Console.WriteLine($"Більше 5 км: {avg3:F2}");
         Console.WriteLine($"Загальне середнє: {totalAverage:F2}");
+
+
+        double totalSumOfSquares = CalculateTotalSumOfSquares(occupancyData, totalAverage);
+        Console.WriteLine($"Загальна сума квадратів відхилень: {totalSumOfSquares:F2}");
     }
 
-    // Функція для обчислення середнього значеннямасиву
     static double CalculateAverage(double[] data)
     {
         double sum = 0;
@@ -35,7 +38,6 @@ class Program
         return sum / data.Length;
     }
 
-    // Функція для обчислення загального середнього значення
     static double CalculateOverallAverage(double[][] data)
     {
         double totalSum = 0;
@@ -51,6 +53,22 @@ class Program
         }
 
         return totalSum / totalCount;
+    }
+
+    static double CalculateTotalSumOfSquares(double[][] data, double overallAverage)
+    {
+        double sumOfSquares = 0;
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            for (int j = 0; j < data[i].Length; j++)
+            {
+                double deviation = data[i][j] - overallAverage;
+                sumOfSquares += deviation * deviation;
+            }
+        }
+
+        return sumOfSquares;
     }
 
 }
